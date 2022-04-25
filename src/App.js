1d916ab6462bb56efc5aed02fe59185d9c4e76fe
vuvuhoang <vuvuhoang99@gmail.com>
2022-04-25 00:24:11 -0400

    build nav bar, landing page, and featured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from './components/Landing';
+import Nav from "./components/Nav";
+import Highlights from './components/Highlights';
+import Featured from './components/Featured';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
